e2386634f43191dc447277fccc634e3349b41dba
Aiman Haziq <63447253+aimanhaziqmy@users.noreply.github.com>
2023-02-12 13:10:51 +0800

    Update configuration.js

--- config/configuration.js
+++ config/configuration.js
@@ -1,7 +1,7 @@
 module.exports = {
-    //mongoDbUrl: 'mongodb+srv://maman:PerinduHebat2020@kolejperindu.ijzi3.gcp.mongodb.net/Perindu_Website?retryWrites=true&w=majority',
+    //mongoDbUrl: 'mongodb+srv://maman:<Password>@kolejperindu.ijzi3.gcp.mongodb.net/Perindu_Website?retryWrites=true&w=majority',
     //mongodb://127.0.0.1:27017/perinduwebs
-    mongoDbUrl: process.env.MONGO_URI || 'mongodb+srv://maman:PerinduHebat2020@kolejperindu.ijzi3.gcp.mongodb.net/Perindu_Website?retryWrites=true&w=majority',
+    mongoDbUrl: process.env.MONGO_URI || 'mongodb+srv://maman:<Password>@kolejperindu.ijzi3.gcp.mongodb.net/Perindu_Website?retryWrites=true&w=majority',
     PORT: process.env.PORT || 3000,
     AdminKey : process.env.ADMIN_KEY || "PerinduHebat2020#",
     
